5ecc46dd163d51589406f7640e6b41e1bb93e379
JhonDevS <sierrazuluaga.j@gmail.com>
2024-07-13 06:41:09 -0500

    feat: creation file route

---
+++ router/auth_routes.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Depends
+from typing import Annotated
+from config import get_firebase_user_from_token
+
+router = APIRouter()
+
+
+@router.get("/userid")
+async def get_userid(user: Annotated[dict, Depends(get_firebase_user_from_token)]):
+    """gets the firebase connected user"""
+    return {"id": user["uid"]}
