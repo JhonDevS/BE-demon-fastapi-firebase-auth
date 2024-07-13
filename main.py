6d122eb57b6b9cdfb5cc2eb7d8a7459d672ec9bc
JhonDevS <sierrazuluaga.j@gmail.com>
2024-07-13 06:39:40 -0500

    feat: modularizacion of logic

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from router import auth_routes
+# pip install firebase-admin
+import firebase_admin
+from fastapi.middleware.cors import CORSMiddleware
+from config import get_settings
 
 app = FastAPI()
+app.include_router(auth_routes.router)
 
+origins = [get_settings().frontend_url]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+firebase_admin.initialize_app()
+print("Current App Name:", firebase_admin.get_app().project_id)
