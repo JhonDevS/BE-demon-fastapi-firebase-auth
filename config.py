8e6816a3fd51c6e5d34c0934df4e7140d0d582d1
JhonDevS <sierrazuluaga.j@gmail.com>
2024-07-13 21:59:54 -0500

    feat: configuration with fire base success

--- config.py
+++ config.py
@@ -13,13 +13,15 @@ from pydantic_settings import BaseSettings
 bearer_scheme = HTTPBearer(auto_error=False)
 
 basedir = pathlib.Path(__file__).parents[1]
-load_dotenv(basedir / ".env")
+completed_dir = basedir / "demo-fastapi-firebase-auth/.env"
+load_dotenv(completed_dir)
 
 
 class Settings(BaseSettings):
     app_name: str = "demo-fastapi-firebase-auth"
     env: str = os.getenv("ENV", "development")
     frontend_url: str = os.getenv("FRONTEND_URL", "NA")
+    credential_fire_base: str = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 
 @lru_cache
@@ -32,6 +34,7 @@ def get_firebase_user_from_token(
         token: Annotated[Optional[HTTPAuthorizationCredentials], Depends(bearer_scheme)],
 ) -> Optional[dict]:
     try:
+        print(token)
         if not token:
             raise ValueError("No token")
         user = verify_id_token(token.credentials)
--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI
 from router import auth_routes
 # pip install firebase-admin
 import firebase_admin
+from firebase_admin import credentials
 from fastapi.middleware.cors import CORSMiddleware
 from config import get_settings
+import os
 
 app = FastAPI()
 app.include_router(auth_routes.router)
 
-origins = [get_settings().frontend_url]
+origins = [get_settings().frontend_url, "http://192.168.1.58:5173"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -17,5 +19,5 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-firebase_admin.initialize_app()
-print("Current App Name:", firebase_admin.get_app().project_id)
+cred = credentials.Certificate(get_settings().credential_fire_base)
+firebase_admin.initialize_app(cred)
