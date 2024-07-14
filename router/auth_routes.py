d1869b48d8e67876fec17cdcc7146c728761a349
JhonDevS <sierrazuluaga.j@gmail.com>
2024-07-14 08:31:16 -0500

    feat: creation service sign out and endpoint

--- router/auth_routes.py
+++ router/auth_routes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from typing import Annotated
 from starlette.responses import JSONResponse
-from service.firebase import verify_user_token, login
+from service.firebase import verify_user_token, login, signup, sign_out
 from service.models.request_login import RequestLogin
 
 router = APIRouter()
@@ -19,3 +19,21 @@ async def post_login(request: RequestLogin):
     password = request.password
     jwt = login(email, password)
     return JSONResponse(content={'token': jwt}, status_code=200)
+
+
+@router.post("/signup", include_in_schema=False)
+async def post_creation_user(request: RequestLogin):
+    email = request.email
+    password = request.password
+    jwt = signup(email, password)
+    return JSONResponse(content={'token': jwt}, status_code=200)
+
+
+# router de sing out
+@router.post("/sign-out")
+async def post_sign_out(user: Annotated[dict, Depends(verify_user_token)]):
+    user_id = user['uid']
+    if not user_id:
+        return JSONResponse(content={'message': 'No valid token'}, status=400)
+    sign_out(user_id)
+    return JSONResponse(content={'message': 'Successfully signed out'}, status_code=200)
--- service/firebase.py
+++ service/firebase.py
@@ -3,7 +3,7 @@ import json
 import pyrebase
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPAuthorizationCredentials
-from firebase_admin.auth import verify_id_token
+from firebase_admin.auth import verify_id_token, revoke_refresh_tokens, create_session_cookie
 from typing import Annotated, Optional
 
 from service.config_firebase import bearer_scheme
@@ -18,7 +18,7 @@ def verify_user_token(
         print(token)
         if not token:
             raise ValueError("No token")
-        user = verify_id_token(token.credentials)
+        user = verify_id_token(token.credentials, check_revoked=True)
         return user
     except Exception:
         raise HTTPException(
@@ -34,3 +34,21 @@ def login(email: str, password: str):
         return user['idToken']
     except Exception:
         raise HTTPException(detail={'message': 'There was an error logging in'}, status_code=400)
+
+
+def signup(email: str, password: str):
+    try:
+        if email is None or password is None:
+            return HTTPException(detail={'message': 'Error! Missing Email or Password'}, status_code=400)
+        user = firebase.auth().create_user_with_email_and_password(email, password)
+        return user['idToken']
+    except Exception:
+        raise HTTPException(detail={'message': 'There was an error signing up'}, status_code=400)
+
+
+def sign_out(uid: str):
+    try:
+        revoke_refresh_tokens(uid)
+        return True
+    except Exception:
+        raise HTTPException(detail={'message': 'There was an error signing out'}, status_code=400)
